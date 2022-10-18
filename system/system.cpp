eda722e38862c63b6ac24732240fa6c085f92436
Koza_jp97@student.itstep.org <Koza_jp97@student.itstep.org>
2022-10-18 14:46:23 +0300

    Add project files.

---
+++ system/system.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	system("md ITEfolder1");
+	system("md ITEfolder2");
+	system("md ITEfolder3");
+	system("md ITEfolder3\\ITEfolder4\\ITEfolder5");
+	system("echo Я вивчаю програмування > doc1.txt");
+	system("more doc1.txt");
+}
